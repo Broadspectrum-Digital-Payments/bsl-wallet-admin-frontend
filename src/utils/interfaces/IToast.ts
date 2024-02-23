ed568d923c5faa6549cf2f71da648e30fb305502
Jonadab Kwamlah <jkwamlah@outlook.com>
2024-02-23 22:18:27 +0000

    fixed toast and kyc pagination

--- src/utils/interfaces/IToast.ts
+++ src/utils/interfaces/IToast.ts
@@ -1,7 +1,7 @@
 import React from "react";
 
 export interface IToast {
-    toastType?: string;
+    toastType: string;
     toastDescription?: string;
     customClasses?: string;
     children?: React.ReactNode
