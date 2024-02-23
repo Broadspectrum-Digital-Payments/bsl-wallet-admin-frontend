33edfcb4c84f0bb39b643b630ff814298642d0a5
Jonadab Kwamlah <jkwamlah@outlook.com>
2024-02-23 07:10:26 +0000

    added suspense fix

--- src/app/auth/verify/page.tsx
+++ src/app/auth/verify/page.tsx
@@ -1,12 +1,16 @@
 'use client'
-import React from 'react';
+import React, {Suspense} from 'react';
 import VerifyAdminContent from "@/components/auth/VerifyAdminContent";
 import AuthLayout from "@/components/layout/AuthLayout";
 
 const VerifyPage: React.FC = () => {
-    return <AuthLayout title="Create Your Password">
-        <VerifyAdminContent/>
-    </AuthLayout>
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <AuthLayout title="Create Your Password">
+                <VerifyAdminContent/>
+            </AuthLayout>
+        </Suspense>
+    )
 };
 
 export default VerifyPage;
--- src/components/auth/VerifyAdminContent.tsx
+++ src/components/auth/VerifyAdminContent.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {useRouter, useSearchParams} from "next/navigation";
 import {resetPassword} from "@/api/auth";
 import {getError} from "@/utils/helpers";
@@ -22,34 +22,37 @@ const VerifyAdminContent: React.FC = () => {
 
     const handleEmailLinkVerification = (password: string) => {
         if (setLoading) setLoading(true);
-        resetPassword(password, token)
-            .then(async (response) => {
-                const feedback = await response.json()
-                if (setLoading) setLoading(false);
+        if (token) {
+            resetPassword(password, token)
+                .then(async (response) => {
+                    const feedback = await response.json()
+                    if (setLoading) setLoading(false);
 
-                if (response.ok && feedback.success) {
-                    const {data} = feedback
-                    if (setAuthenticatedAdmin) setAuthenticatedAdmin({
-                        externalId: data.externalId,
-                        email: data.email,
-                        name: data.name,
-                        status: data.status,
-                        userType: data.userType,
-                        bearerToken: data.bearerToken,
-                        createdAt: data.createdAt
-                    })
+                    if (response.ok && feedback.success) {
+                        const {data} = feedback
+                        if (setAuthenticatedAdmin) setAuthenticatedAdmin({
+                            externalId: data.externalId,
+                            email: data.email,
+                            name: data.name,
+                            status: data.status,
+                            userType: data.userType,
+                            bearerToken: data.bearerToken,
+                            createdAt: data.createdAt
+                        })
 
-                    if (setIsAuthenticated) setIsAuthenticated(true)
-                    if (setFirstTimeLogin) setFirstTimeLogin(true)
-                    if (setActiveSidebarMenu) setActiveSidebarMenu(mainMenuItemsList[0]);
-                    return router.push('/overview')
-                } else
-                    return setError(getError(feedback))
-            })
-            .catch((error) => {
-                setLoading(false)
-                setError(error.message)
-            })
+                        if (setIsAuthenticated) setIsAuthenticated(true)
+                        if (setFirstTimeLogin) setFirstTimeLogin(true)
+                        if (setActiveSidebarMenu) setActiveSidebarMenu(mainMenuItemsList[0]);
+                        return router.push('/overview')
+                    } else
+                        return setError(getError(feedback))
+                })
+                .catch((error) => {
+                    setLoading(false)
+                    setError(error.message)
+                })
+        }
+
     }
 
     const handleError = (error: string) => setError(error)
