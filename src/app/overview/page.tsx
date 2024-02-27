a4c39f1d6250f8679d9f5c6fd7597e037f440157
Jonadab Kwamlah <jkwamlah@outlook.com>
2024-02-27 02:07:54 +0000

    Fixed authentication refresh bug

--- src/app/overview/page.tsx
+++ src/app/overview/page.tsx
@@ -2,16 +2,8 @@
 import React, {useEffect} from 'react'
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import OverviewContent from "@/components/overview/OverviewContent";
-import mainMenuItemsList from "@/components/layout/MainMenuItemsList";
-import {useAdminStore} from "@/store/AdminStore";
 
 const OverviewPage: React.FC = () => {
-    const {setActiveSidebarMenu} = useAdminStore()
-    useEffect(() => {
-        if (setActiveSidebarMenu) setActiveSidebarMenu(mainMenuItemsList[0])
-    }, []);
-
-
     return (
         <DashboardLayout>
             <OverviewContent/>
